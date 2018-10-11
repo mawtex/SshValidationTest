5a84001903751ebd044e0b20b2c7c685c7738fb5
admin <admin>
2018-10-11 15:26:14 +0200

    User publish from CMS

--- .deploy/local-website-deployment.ps1
+++ .deploy/local-website-deployment.ps1
@@ -122,7 +122,8 @@ Get-ChildItem "$deploymentTemp\*" -include $allEnvFilenamePatterns -Recurse -For
 #
 [console]::WriteLine("Robocopy to target, pure copy only, affected files:")
 robocopy /e $deploymentTemp $deploymentTarget /r:1 /w:1 /njh /ndl /nc /ns /np 
-[console]::WriteLine("Robocopy to target, mirroring (excluding $leaveOnDeploymentDirectories), affected files:")
+[console]::WriteLine("Robocopy to target, mirroring (excluding $leaveOnDeploymentDirectories), affected files / directories:")
+[console]::WriteLine("    (Be aware that directories listed by RoboCopy below are likely being deleted and will cause the IIS AppPool to recycle)")
 robocopy /mir $deploymentTemp $deploymentTarget /r:3 /w:5 /njh /ndl /nc /ns /np /xd @leaveOnDeploymentDirectories
 [console]::WriteLine("    Completed custom part of deployment")
 
